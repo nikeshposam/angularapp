ccabcb6d093f14e94f1bdccc0d61e3a6880bf9df
Nikesh Posam <nikeshposam@github.com>
2018-04-05 01:14:07 +0530

    message added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from "../hero.service";
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,14 +12,15 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
